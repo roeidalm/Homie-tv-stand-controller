488c23efc73110a0617ca0dd57fe83d5e6ef1448
Ramzi <dalmedigos@gmail.com>
2020-10-03 21:15:15 +0300

    add the decoded type

--- src/secTask.h
+++ src/secTask.h
@@ -1,4 +1,5 @@
 
+#include <Arduino.h>
 TaskHandle_t Task1;
 
 #if defined(ESP32)
@@ -8,16 +9,40 @@ int IR_RECEIVE_PIN = 11;
 #endif
 IRrecv irrecv(IR_RECEIVE_PIN);
 decode_results results;
+HomieNode irRecvNode("irRecv", "IrReceiver", "reader");
 
 void Task1code(void *parameter)
 {
     irrecv.enableIRIn(); // Start the receiver
 
+    irRecvNode.advertise("irRead_Cmd").setName("IRRead_Cmd").setDatatype("float");
+    irRecvNode.advertise("ReadCmd_decode_type").setName("ReadCmd_decode_type").setDatatype("string");
     for (;;)
     {
         if (irrecv.decode(&results))
         {
             Homie.getLogger() << "recieve IR: " << results.value << endl;
+            String res = "";
+            switch (results.decode_type)
+            {
+            case NEC:
+                res = "NEC";
+                break;
+            case SONY:
+                res = "SONY";
+                break;
+            case RC5:
+                res = "RC5";
+                break;
+            case RC6:
+                res = "RC6";
+                break;
+            default:
+                res = "UNKNOWN";
+                break;
+            }
+            irRecvNode.setProperty("irRead_Cmd").send(String(results.value));
+            irRecvNode.setProperty("readCmd_decode_type").send(res);
 
             irrecv.resume(); // Receive the next value
         }
